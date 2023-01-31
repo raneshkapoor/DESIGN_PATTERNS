f4bd105d9dc8e62129432172770fdd44d164c7a4
Ranesh Kapoor <raneshkapoor@ymail.com>
2023-02-01 00:04:01 +0530

    Added Factory Pattern Main Class

--- design-patterns/src/main/java/com/designs/patterns/creational/factory/FactoryPatternMain.java
+++ design-patterns/src/main/java/com/designs/patterns/creational/factory/FactoryPatternMain.java
@@ -1,10 +1,21 @@
 package com.designs.patterns.creational.factory;
 
+import com.designs.patterns.creational.factory.pizzas.Pizza;
+import com.designs.patterns.creational.factory.pizzas.PizzaFactory;
+import com.designs.patterns.creational.factory.pizzas.enums.PizzaType;
+import com.designs.patterns.creational.factory.pizzas.impl.PizzaFactoryNewYork;
+
 public class FactoryPatternMain {
 
     public static void main(String[] args) {
 
+        PizzaFactory factory = new PizzaFactoryNewYork();
 
+        Pizza pizza = factory.getPizza(PizzaType.FARMHOUSE);
+
+        pizza.pizzaBase();
+        pizza.pizzaSauce();
+        pizza.pizzaToppings();
 
     }
 
